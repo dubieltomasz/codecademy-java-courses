3cdcb7808ef917dc956e4fdac630f6212d15a0df
Tomasz Dubiel <dubieltomasz420@gmail.com>
2023-06-18 11:45:24 +0200

    Create Variables_9.java

---
+++ Variables/Variables_9.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name = "Tomek";
+    int age = 19;
+    double desiredSalary = 48000.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+	}
+}
