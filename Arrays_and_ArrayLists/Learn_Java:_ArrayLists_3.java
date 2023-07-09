192e0c56c1d958bced3151f145ee82c7a92e1edb
Tomasz Dubiel <dubieltomasz420@gmail.com>
2023-07-09 16:37:35 +0200

    Create Learn_Java:_ArrayLists_3.java

---
+++ Arrays_and_ArrayLists/Learn_Java:_ArrayLists_3.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+class ToDos {
+  public static void main(String[] args) {
+    ArrayList<String> toDoList = new ArrayList<String>();
+    String toDo1 = "Water plants";
+
+    // Add more to-dos here:
+    String toDo2 = "Feed dog";
+    String toDo3 = "Wash dishes";
+    
+    // Add to-dos to toDoList
+    toDoList.add(toDo1);
+    toDoList.add(toDo2);
+    toDoList.add(toDo3);
+    
+    System.out.println(toDoList);
+  }
+}
