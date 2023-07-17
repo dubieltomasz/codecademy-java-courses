ab7b060be842c317de203e3506502b68ed9819e4
Tomasz Dubiel <dubieltomasz420@gmail.com>
2023-07-17 18:31:31 +0200

    Create String_Methods_3.java

---
+++ String_Methods/String_Methods_3.java
@@ -0,0 +1,11 @@
+public class CombineNames {
+  
+	public static void main(String[] args) {
+    // Add your first names:
+    String firstName = "firstName";  
+    String lastName = "lastName";
+
+    // What's the full name?
+    System.out.println(firstName.concat(lastName));
+  }
+}
