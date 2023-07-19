7c64c75f71b00884fd545a93f920a8c0db2355eb
Tomasz Dubiel <dubieltomasz420@gmail.com>
2023-07-19 21:49:22 +0200

    Create String_Methods_5.java

---
+++ String_Methods/String_Methods_5.java
@@ -0,0 +1,9 @@
+public class MorseCode {
+  
+  public static void main(String[] args) {
+    String code = ".... .. / .. .----. -- / ... --- -. -. -.-- / .- -. -.. / .. / .-.. .. -.- . / - --- / . .- - / .-. .- -- . -.";
+
+    // Write the code:
+    System.out.println(code.indexOf(". .- -"));
+  }
+}
